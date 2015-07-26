efea5d3011096f802329f16bebe488b75c1c13c6
Franklin Moody <fmoody@moodman.org>
2015-07-26 13:42:49 -0400

    Initial commit

---
+++ playlister.py
@@ -0,0 +1,132 @@
+#!./bin/python
+
+import httplib2
+import os
+import sys
+
+# TO REMOVE LATER
+import pprint
+# TO REMOVE LATER
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
+# the {{ Google Cloud Console }} at
+# {{ https://cloud.google.com/console }}.
+# Please ensure that you have enabled the YouTube Data API for your project.
+# For more information about using OAuth2 to access the YouTube Data API, see:
+#   https://developers.google.com/youtube/v3/guides/authentication
+# For more information about the client_secrets.json file format, see:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# This variable defines a message to display if the CLIENT_SECRETS_FILE is
+# missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+   %s
+
+with information from the {{ Cloud Console }}
+{{ https://cloud.google.com/console }}
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account.
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+def main():
+
+    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
+    message=MISSING_CLIENT_SECRETS_MESSAGE,
+    scope=YOUTUBE_READ_WRITE_SCOPE)
+    
+    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    credentials = storage.get()
+  
+    if credentials is None or credentials.invalid:
+        flags = argparser.parse_args()
+        credentials = run_flow(flow, storage, flags)
+    
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    http=credentials.authorize(httplib2.Http()))
+
+    playlist_ids = get_playlist_ids(youtube)
+    
+    print("Play list ids:\n")
+    pp.pprint(playlist_ids)
+    print("\n\n")
+
+    print("Playlist contents:\n")
+    for playlist in playlist_ids:
+        print "Play list title: ", playlist['title'], " (", playlist['id'], ")"
+        playlist_contents = get_playlist_contents(youtube, playlist['id'])
+        #pp.pprint(playlist_contents)
+        print "Count is ", len(playlist_contents)
+        for item in playlist_contents:
+            print "\tVideo title: ", item['title'].encode('ascii', 'ignore'), " (", item['id'], ")"
+            #print "\tVideo title: (", item['id'], ")"
+        print "\n"
+
+    playlist_contents = get_playlist_contents(youtube, 'PLR21k8SuCtZdOJbTyeQ3vXy-y8Ipi3q_E')
+    #pp.pprint(dir(youtube))
+
+
+def get_playlist_contents(youtube, id):
+
+    playlist_contents = []
+    playlist_items = youtube.playlistItems().list(part='snippet', playlistId=id, maxResults=50).execute()
+    playlist_contents += [{'title' : item['snippet']['title'], 'id' : item['id']} for item in playlist_items['items']]
+    #print "*** playlist_items ***"
+    #pp.pprint(playlist_items)
+    
+    while ('nextPageToken' in playlist_items):
+        print "Page Token: ", playlist_items['nextPageToken']
+        playlist_items = youtube.playlistItems().list(part='snippet', playlistId=id, maxResults=50, pageToken=playlist_items['nextPageToken']).execute()
+        #pp.pprint(playlist_items)
+        playlist_contents += [{'title' : item['snippet']['title'], 'id' : item['id']} for item in playlist_items['items']]
+
+    #pp.pprint(playlist_contents)
+    #print "***  ***"
+    return playlist_contents
+
+def get_playlist_ids(youtube):
+
+    # This code will return a list of playlist ids and titles for a user
+    playlists_request = youtube.playlists().list(part='id,snippet',mine=True).execute()
+
+    playlists = [{'id' : playlist['id'], 'title' : playlist['snippet']['title']} for playlist in playlists_request['items']]
+
+    # Get watch later playlist via looking up the user's channel
+    channels_listing = youtube.channels().list(part="contentDetails", mine=True).execute()
+    playlists.append({'id':get_watchlater_playlist_id(youtube), 'title': u'Watch Later'})
+
+    #playlists.append(watchLater_list)
+    
+    return playlists
+
+def get_watchlater_playlist_id(youtube):
+    # Get watch later playlist via looking up the user's channel
+    channels_listing = youtube.channels().list(part="contentDetails", mine=True).execute()
+    return channels_listing['items'][0]['contentDetails']['relatedPlaylists']['watchLater']
+
+if __name__ == '__main__':
+    pp = pprint.PrettyPrinter()
+    main()
+    
