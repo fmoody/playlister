e8a4db159dd08c1a9e99ff22d4c37f754e728f6b
fmoody <fmoody@moodman.org>
2017-05-21 11:38:23 -0400

    A bit of a revamp.  Basic functionality is now completed for listing/adding/removing playlists as well as listing videos in a playlist and adding/removing those videos from a given playlist.

--- playlister.py
+++ playlister.py
@@ -1,66 +1,99 @@
 #!/usr/bin/env python
 
 import argparse
-import pprint
 import oauth2client.tools
 
-from google_auth_code import *
-from tooling import *
+from google_auth_code import get_youtube_connection
+from tooling import display_playlist_list, get_playlist_id_by_title, add_playlist, \
+                    delete_playlist_by_title, copy_videos_from_playlist1_to_playlist2, \
+                    list_playlist_contents, add_video_to_playlist, remove_video_from_playlist
 
 
 def main():
 
-    opt_parser = argparse.ArgumentParser(description="manipulate your youtube playlists", parents=[oauth2client.tools.argparser])
-    opt_parser.add_argument("--playlist", help="The name of the playlist", required=True)
-
-    action_group = opt_parser.add_mutually_exclusive_group(required=True)
-    action_group.add_argument("--list", help="List a playlist", action="store_true")
-    action_group.add_argument("--add", help="Add a playlist", action="store_true")
-    action_group.add_argument("--rm", help="Remove a playlist", action="store_true")
-    action_group.add_argument("--cp", help="Copy PLAYLIST to CP", default=False)
-
-    opt_parser.add_argument("--description", help="Description of the playlist")
-    opt_parser.add_argument("--privacy_status", help="Privacy Status",
-                            choices=['public', 'private', 'unlisted'], default='private')
-
-    args = opt_parser.parse_args()
+    args = construct_arg_parser()
 
     youtube = get_youtube_connection(args)
 
-    if args.list:
-        playlist_ids = get_playlist_id_by_title(youtube, args.playlist)
-        for playlist_id in playlist_ids:
-            pp.pprint(get_playlist_contents(youtube, playlist_id['id']))
-    elif args.add:
+    if args.action == "list_playlists":
+        display_playlist_list(youtube, args.detail, args.match)
+    elif args.action == "add_playlist":
         add_playlist(youtube, args.playlist, args.description, args.privacy_status)
-    elif args.rm:
+    elif args.action == "rm_playlist":
         delete_playlist_by_title(youtube, args.playlist)
-    elif args.cp:
-        print("placeholder")
+    elif args.action == "cp_playlist":
+        copy_videos_from_playlist1_to_playlist2(youtube, args.from_playlist, args.to_playlist)
+    elif args.action == "list":
+        list_playlist_contents(youtube, args.playlist, args.detail)
+    elif args.action == "add":
+        playlist_id = get_playlist_id_by_title(youtube, args.playlist, True)[0]['id']
+        add_video_to_playlist(youtube, playlist_id, args.videoId)
+    elif args.action == "rm":
+        playlist_id = get_playlist_id_by_title(youtube, args.playlist)[0]['id']
+        remove_video_from_playlist(youtube, playlist_id, args.videoId)
+    else:
+        print "Error, no command executed."
 
 
-def print_playlist_info(youtube):
-    """ Print a "pretty" dump of a users playlists and their contents. """
+def construct_arg_parser():
+    opt_parser = argparse.ArgumentParser(description="Manipulate your youtube playlists",
+                                         parents=[oauth2client.tools.argparser])
 
-    playlists = get_playlists(youtube)
+    subparsers = opt_parser.add_subparsers(title="Playlister",
+                                           dest="action")
 
-    print("Play list ids:\n")
-    for playlist in playlists:
-        print "Title: ", playlist['title']
-    print("\n\n")
+    playlists_parser = subparsers.add_parser("list_playlists",
+                                             help="List Playlists")
+    playlists_parser.add_argument("--match",
+                                  help="Case-sensitive text to match")
+    playlists_parser.add_argument("--detail",
+                                  choices=["title", "id"],
+                                  action="append")
 
-    print("Playlist contents:\n")
-    for playlist in playlists:
-        print "Play list title: ", playlist['title'], " (", playlist['id'], ")"
-        playlist_contents = get_playlist_contents(youtube, playlist['id'])
-        print "Count is ", len(playlist_contents)
-        for item in playlist_contents:
-            print "\tVideo title: ", item['snippet']['title'].encode('ascii', 'ignore'),
-            " (", item['id'], ")"
-        print "\n"
+    add_playlist_parser = subparsers.add_parser("add_playlist",
+                                                help="Add a playlist")
+    add_playlist_parser.add_argument("playlist")
+    add_playlist_parser.add_argument("--description",
+                                     help="Description of the playlist")
+    add_playlist_parser.add_argument("--privacy_status",
+                                     help="Privacy Status",
+                                     choices=['public', 'private', 'unlisted'],
+                                     default='private')
 
-    return
+    rm_playlist_parser = subparsers.add_parser("rm_playlist",
+                                               help="Remove a playlist")
+    rm_playlist_parser.add_argument("playlist")
+
+    cp_playlist_parser = subparsers.add_parser("cp_playlist",
+                                               help="Copy a playlist to another playlist")
+    cp_playlist_parser.add_argument("from_playlist")
+    cp_playlist_parser.add_argument("to_playlist")
+
+    list_parser = subparsers.add_parser("list",
+                                        help="List videos in a playlist")
+    list_parser.add_argument("playlist")
+    list_parser.add_argument("--detail",
+                             choices=["title", "id", "debug"],
+                             action="append")
+
+    add_video_parser = subparsers.add_parser("add",
+                                             help="Add a video to a playlist")
+    add_video_parser.add_argument("playlist",
+                                  help="Playlist to add video to")
+    add_video_parser.add_argument("videoId",
+                                  help="Video id to add to the playlist")
+
+    rm_video_parser = subparsers.add_parser("rm",
+                                            help="Remove a video to a playlist")
+    rm_video_parser.add_argument("playlist",
+                                 help="Playlist to remove video from")
+    rm_video_parser.add_argument("videoId",
+                                 help="Video id to remove from the playlist")
+
+    args = opt_parser.parse_args()
+
+    return args
+
 
 if __name__ == '__main__':
-    pp = pprint.PrettyPrinter()
     main()
--- tooling.py
+++ tooling.py
@@ -1,6 +1,45 @@
-import pprint
+def display_playlist_list(youtube, detail, match):
+    """ Display the list of playlists associated with the account """
 
-pp = pprint.PrettyPrinter()
+    if detail is None:
+        detail = ["title"]
+
+    playlists = get_playlists(youtube)
+
+    for playlist in playlists:
+        if match is None or match in playlist['title']:
+            for detail_item in detail:
+                if detail_item == 'title':
+                    detail_output = "\""
+                    detail_output += playlist[detail_item]
+                    detail_output += "\""
+                else:
+                    detail_output = playlist[detail_item]
+
+                print detail_output,
+            print
+    return
+
+
+def list_playlist_contents(youtube, playlist, detail):
+    if detail is None:
+        detail = ["title", "id"]
+
+    playlist_ids = get_playlist_id_by_title(youtube, playlist)
+    for playlist_id in playlist_ids:
+        playlist_contents = get_playlist_contents(youtube, playlist_id['id'])
+        for playlist_entry in playlist_contents:
+            entry_output = ""
+            if "title" in detail:
+                entry_output += "\""
+                entry_output += playlist_entry['snippet']['title']
+                entry_output += "\" "
+            if "id" in detail:
+                entry_output += playlist_entry['snippet']['resourceId']['videoId']
+            if "debug" in detail:
+                entry_output += str(playlist_entry)
+            print(entry_output)
+    return
 
 
 def delete_playlist_by_title(youtube, title):
@@ -11,10 +50,13 @@ def delete_playlist_by_title(youtube, title):
     return
 
 
-def get_playlist_id_by_title(youtube, title):
+def get_playlist_id_by_title(youtube, title, create_missing_playlist=False):
     """ Retrieve a list of playlists (ids and titles) that match the given title. """
     playlists = get_playlists(youtube)
     named_playlists = filter(lambda playlist: playlist['title'] == title, playlists)
+    if (not named_playlists and create_missing_playlist):
+        request_response = add_playlist(youtube, title, title, "private")
+        named_playlists = [dict(id=request_response['id'], title=title)]
     return named_playlists
 
 
@@ -30,7 +72,8 @@ def add_playlist(youtube, title, description, privacy_status):
                                                   body=dict(
                                                       snippet=dict(title=title,
                                                                    description=description),
-                                                      status=dict(privacyStatus=privacy_status))).execute()
+                                                      status=dict(privacyStatus=privacy_status)
+                                                  )).execute()
     return request_response
 
 
@@ -51,18 +94,18 @@ def get_playlist_contents(youtube, desired_playlist_id):
                                                       part='snippet',
                                                       fields=fields_list,
                                                       maxResults=50,
-                                                      pageToken=playlist_items['nextPageToken']).execute()
+                                                      pageToken=playlist_items['nextPageToken']
+                                                      ).execute()
         playlist_contents += [item for item in playlist_items['items']]
 
     return playlist_contents
 
 
 def get_playlists(youtube):
-    """ Given an authenticated youtube object, return a list of playlist ids and titles
-    (including the 'watch later' playlist) """
+    """ Given an authenticated youtube object, return a list of playlist ids and titles """
 
     playlists = []
-    
+
     playlists_request = youtube.playlists().list(part='id,snippet', mine=True)
 
     while(playlists_request is not None):
@@ -71,13 +114,70 @@ def get_playlists(youtube):
                       for playlist in response['items']]
         playlists_request = youtube.playlists().list_next(playlists_request, response)
 
-    # Add the 'Watch Later' playlist to the normal set of playlists
-    playlists.append({'id': get_watchlater_playlist_id(youtube), 'title': u'Watch Later'})
-
     return playlists
 
-def get_watchlater_playlist_id(youtube):
-    """ Given an authenticate youtube object, retrieve the watch later playlist id
-    from the user's channel info. """
-    channels_listing = youtube.channels().list(part="contentDetails", mine=True).execute()
-    return channels_listing['items'][0]['contentDetails']['relatedPlaylists']['watchLater']
+
+def add_video_to_playlist(youtube, playlist_id, videoIdToAdd):
+    """ Add a video to playlist """
+
+    request_response = youtube.playlistItems().insert(part="snippet",
+                                                      body=dict(
+                                                          snippet=dict(
+                                                              playlistId=playlist_id,
+                                                              resourceId=dict(
+                                                                  kind="youtube#video",
+                                                                  videoId=videoIdToAdd
+                                                                  )
+                                                              )
+                                                          )
+                                                      ).execute()
+
+    return request_response
+
+
+def remove_video_from_playlist(youtube, playlist_id, videoIdToRM):
+    """ Remove a video from playlist """
+    playlistItems = get_playlist_contents(youtube, playlist_id)
+
+    unwanted_playlistItems = filter(lambda item:
+                                    item['snippet']['resourceId']['videoId'] == videoIdToRM,
+                                    playlistItems)
+
+    if unwanted_playlistItems:
+        for item in unwanted_playlistItems:
+            youtube.playlistItems().delete(id=item['id']).execute()
+
+
+def copy_videos_from_playlist1_to_playlist2(youtube, playlist1_title, playlist2_title):
+    playlist1_id = get_playlist_id_by_title(youtube, playlist1_title)
+    playlist2_id = get_playlist_id_by_title(youtube, playlist2_title, True)[0]['id']
+
+    playlist1_contents = get_playlist_contents(youtube, playlist1_id[0]['id'])
+
+    for video in playlist1_contents:
+        add_video_to_playlist(youtube, playlist2_id, video['snippet']['resourceId']['videoId'])
+
+    return
+
+
+def print_playlist_info(youtube):
+    """ Print a "pretty" dump of a users playlists and their contents. """
+
+    playlists = get_playlists(youtube)
+
+    print("Play list ids:\n")
+    for playlist in playlists:
+        print "Title: ", playlist['title']
+    print("\n\n")
+
+    print("Playlist contents:\n")
+    for playlist in playlists:
+        print "Play list title: ", playlist['title'], " (", playlist['id'], ")"
+        playlist_contents = get_playlist_contents(youtube, playlist['id'])
+        print "Count is ", len(playlist_contents)
+        for item in playlist_contents:
+            print "\tVideo title: ", item['snippet']['title'].encode('ascii', 'ignore'),
+            " (", item['id'], ")"
+        print "\n"
+
+    return
